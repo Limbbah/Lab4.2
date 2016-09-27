65cf67f3e8130bc8d63298e552ad66746e291168
Harri Hyvärinen <Harri Hyvrinen>
2016-09-27 15:24:33 +0300

    changebalance lisätty

--- MyApp.java
+++ MyApp.java
@@ -5,12 +5,14 @@ public static void main(String[] args) {
 	Product product = new Product("Edam", 3.3, 120);
 	System.out.println("Product value is " + product.countValue());
 	product.printProduct();
+	amount.changeBalance();
 	}
 	}
 class Product {
 	private String name;
 	private double price;
 	private int amount; //Amount in storage
+	
 
 public Product(String name, double price, int amount) {
 	this.name = name;
@@ -20,6 +22,9 @@ public Product(String name, double price, int amount) {
 public double countValue() {
 	return(amount * price);
 	}
+public int changeBalance(int newvalue){
+	this.amount = amount--;
+}
 public void printProduct() {
 	System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
 	}
